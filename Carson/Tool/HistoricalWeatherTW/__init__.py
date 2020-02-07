04b94634ebec5a312bb7eb8974d278d367e4ad34
Carson <jackparadise520a@gmail.com>
2020-02-07 15:18:59 +0800

    Add an example to this file so users can get started quickly.

--- Carson/Tool/HistoricalWeatherTW/__init__.py
+++ Carson/Tool/HistoricalWeatherTW/__init__.py
@@ -1,32 +1,15 @@
 __all__ = ('collect_weather_tw', )
 __author__ = ['Carson', 'Sign.X.er']
-__version__ = '4.0.0'
+__version__ = '4.1.0'
 
-try:
-    from .src.HistoricalWeatherTW import collect_weather_tw, QueryFormat
-except ModuleNotFoundError:
-    from src.HistoricalWeatherTW import collect_weather_tw, QueryFormat
+
+from .src.HistoricalWeatherTW import collect_weather_tw, QueryFormat
 from datetime import datetime
 from pathlib import Path
 import os
 
 from argparse import ArgumentParser, RawTextHelpFormatter
 import yaml
-
-DESCRIPTION = 'This script is to crawl the information of CWB Observation Data Inquire System'
-script_description = '\n'.join([desc for desc in
-                                ['\n',
-                                 f'python module.py [CONFIG.YAML]',
-                                 ]])
-
-arg_parser = ArgumentParser(description=DESCRIPTION,
-                            usage=script_description, formatter_class=RawTextHelpFormatter)
-
-arg_parser.add_argument('config', default='config/config.yaml',
-                        help="input the path of ``config.yaml``")
-g_args = arg_parser.parse_args()
-with open(Path(g_args.config)) as f:
-    g_config = yaml.load(f.read(), Loader=yaml.SafeLoader)
 
 
 def get_historical_weather_tw():
@@ -51,4 +34,18 @@ def get_historical_weather_tw():
 
 
 if __name__ == '__main__':
+    DESCRIPTION = 'This script is to crawl the information of CWB Observation Data Inquire System'
+    script_description = '\n'.join([desc for desc in
+                                    ['\n',
+                                     f'python module.py [CONFIG.YAML]',
+                                     ]])
+
+    arg_parser = ArgumentParser(description=DESCRIPTION,
+                                usage=script_description, formatter_class=RawTextHelpFormatter)
+
+    arg_parser.add_argument('-c', '--config', default=Path(__file__).parent / 'config/config.yaml',
+                            help="input the path of ``config.yaml``")
+    g_args = arg_parser.parse_args()
+    with open(Path(g_args.config)) as f:
+        g_config = yaml.load(f.read(), Loader=yaml.SafeLoader)
     get_historical_weather_tw()
